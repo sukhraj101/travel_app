c5374497811061d50025603d7b0a609d5d3bb3ab
Sukhrajsingh <sukhrajsingh101@gmail.com>
2024-07-16 04:51:30 +0530

    Home Page

--- src/components/home/components/citylist/CityList.tsx
+++ src/components/home/components/citylist/CityList.tsx
@@ -106,14 +106,14 @@ const CityList = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <button className="carousel-control-prev" type="button" data-bs-target="#citylistslider" data-bs-slide="prev">
-                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span className="visually-hidden">Previous</span>
-                                </button>
-                                <button className="carousel-control-next" type="button" data-bs-target="#citylistslider" data-bs-slide="next">
-                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span className="visually-hidden">Next</span>
-                                </button>
+                                <div className="sliders-controllers d-flex justify-content-between position-absolute pe-none top-50 start-50">
+                                    <button className="carousel-control-prev position-static w-auto pe-auto" type="button" data-bs-target="#citylistslider" data-bs-slide="prev">
+                                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    </button>
+                                    <button className="carousel-control-next position-static w-auto pe-auto" type="button" data-bs-target="#citylistslider" data-bs-slide="next">
+                                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
